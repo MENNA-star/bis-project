9a71c5ea9db1a3249a553ebd8d497e07118b116c
MENNA-star <mennausama024@gmail.com>
2024-12-15 22:16:35 +0200

    Add files via upload

---
+++ manona/script.js
@@ -0,0 +1,8 @@
+document.getElementById('navToggle').addEventListener('click', function() {
+    var navMenu = document.getElementById('navMenu');
+    if (navMenu.style.display === 'block') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'block';
+    }
+});
